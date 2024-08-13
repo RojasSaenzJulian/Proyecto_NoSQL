35f128be2b8db3a0e32926e8b34d4dcc93b4b0ad
Dennis Sánchez Morales <dennissnnuu@gmail.com>
2024-08-13 11:28:52 -0600

    Cambios necesarios
    Se crea la cantidad completa de pestañas necesarias y la fluidez de la página entre pestañas

--- server.js
+++ server.js
@@ -12,9 +12,41 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-/*app.get('/planeacion', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'planeacion.html'));
-}); Ejemplo para cuando se agregue otra pag*/
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'editarPerfil.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'instalaciones.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notificaciones.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'perfil.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'register.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'sobreNosotros.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'tratamientos.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
